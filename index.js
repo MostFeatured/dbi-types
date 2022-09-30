b25cb515379d8085fb3dc6da5a0dec50f26bcf19
Erdem <74366943+ErdemGKSL@users.noreply.github.com>
2022-09-30 17:45:47 +0300

    first

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 const path = require("path")
 const fs = require("fs");
-const LocaleFilePath = path.resolve(process.cwd(), "./node_modules/@mostfeatured/dbi/dist/types/Locale.d.ts");
+const NamespaceDataLocation = path.resolve(process.cwd(), "./node_modules/@mostfeatured/dbi/generated/namespaceData.d.ts");
 
-/**
- * @param {import("@mostfeatured/dbi/dist/types/Locale").DBILocale} locale 
- * @returns {Promise<Boolean>}
- */
-module.exports.overwriteLocaleTypes = async function overwriteLocaleTypes(locale) {
+module.exports.setNamespaceDataTypes = async function setNamespaceDataTypes(...dbis) {
+  if (dbis.length) {
+
+  }
+  const locale = ";";
   let dataAsString = JSON.stringify(locale.data || locale, (key, value) => typeof value === "string" || typeof value === "function" ? "(...replacers) => string" : value, 2)
     .replace(/"([a-zA-ZğüşiöçĞÜŞİÖÇıİ_][a-zA-Z0-9ğüşiöçĞÜŞİÖÇıİ_]*)"((: {)|(: "([^"]|\\")*[^\\]"))/g, "$1$2")
     .replace(/\"\(\.\.\.replacers\) \=\> string\"/g, "(...replacers: string[]) => string");
-  const localeFileContent = await fs.promises.readFile(LocaleFilePath, "utf-8");
-  const replacedContent = localeFileContent.replace(
-    /export interface LangObject \{.*\}\nexport interface LangConstructorObject \{/s,
-    `export interface LangObject ${dataAsString}\nexport interface LangConstructorObject \{`
-  )
-  await fs.promises.writeFile(LocaleFilePath, replacedContent);
-  return true
 }
+
